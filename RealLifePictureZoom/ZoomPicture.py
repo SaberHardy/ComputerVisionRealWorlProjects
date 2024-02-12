3a0ca85338a84b060c82571d4b072e4042c55bee
Consultant <samirsamado5@gmail.com>
2024-02-11 21:27:50 -0500

    resize the image with hands

--- RealLifePictureZoom/ZoomPicture.py
+++ RealLifePictureZoom/ZoomPicture.py
@@ -16,6 +16,7 @@ while True:
     hands, img = detector.findHands(img)
 
     image_to_zoom = cv2.imread("../Resources/images/Readable.jpg")
+
     if len(hands) == 2:
         hand1 = hands[0]
         hand2 = hands[1]
@@ -34,10 +35,18 @@ while True:
 
             scale = int((length - start_distance) // 2)
             cx, cy = info[4:]
-        else:
-            start_distance = None
+    else:
+        start_distance = None
 
-    cv2.imshow("Image", img)
+    try:
+        h1, w1, _ = image_to_zoom.shape
+        new_height, new_width = ((h1 + scale) // 2) * 2, ((w1 + scale) // 2) * 2
+        image_to_zoom = cv2.resize(image_to_zoom, (new_height, new_width))
+        img[cy - new_height // 2: cy + new_height // 2, cx - new_width // 2: cx + new_width] = image_to_zoom
+    except:
+        pass
+
+    cv2.imshow("Image", image_to_zoom)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
