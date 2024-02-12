42b236c9b014bb04abc6faf58b42069d50e723e6
Consultant <samirsamado5@gmail.com>
2024-02-11 21:05:16 -0500

    read hands

---
+++ RealLifePictureZoom/ZoomPicture.py
@@ -0,0 +1,24 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+detector = HandDetector(detectionCon=0.7)
+
+start_distance = None
+scale = 0
+cx, cy = 200, 200
+
+while True:
+    success, img = cap.read()
+    hands, img = detector.findHands(img)
+
+    cv2.imshow("Image", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
