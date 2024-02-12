f46f060fa9f2522403f427ff326329467c2bfa37
Consultant <samirsamado5@gmail.com>
2024-02-11 21:15:16 -0500

    find the distance between two hands

--- RealLifePictureZoom/ZoomPicture.py
+++ RealLifePictureZoom/ZoomPicture.py
@@ -15,6 +15,28 @@ while True:
     success, img = cap.read()
     hands, img = detector.findHands(img)
 
+    image_to_zoom = cv2.imread("../Resources/images/Readable.jpg")
+    if len(hands) == 2:
+        hand1 = hands[0]
+        hand2 = hands[1]
+
+        hand1_fingers = detector.fingersUp(hand1)
+        hand2_fingers = detector.fingersUp(hand2)
+        if hand1_fingers == [1, 1, 0, 0, 0] and hand2_fingers == [1, 1, 0, 0, 0]:
+            landmarks_list1 = hand1['lmList']
+            landmarks_list2 = hand2['lmList']
+
+            if start_distance is None:
+                length, info, img = detector.findDistance(hand1['center'], hand2['center'], img)
+                start_distance = length
+
+            length, info, img = detector.findDistance(hand1['center'], hand2['center'], img)
+
+            scale = int((length - start_distance) // 2)
+            cx, cy = info[4:]
+        else:
+            start_distance = None
+
     cv2.imshow("Image", img)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
